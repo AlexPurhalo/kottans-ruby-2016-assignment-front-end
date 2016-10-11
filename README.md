0736908c025e1c3b8cb9b389e4f2dd8798198946
AlexPurhalo <alexpurhalo@gmail.com>
2016-10-11 15:55:37 +0300

    data-holding-new-message-form

--- README.md
+++ README.md
@@ -6,5 +6,7 @@
 
 <h3>Progress</h3>
 <ul>
-	<li>visit http://localhost:8080 and checkout form for message creating</li>	
+	<li>visit http://localhost:8080</li>
+	<li>enter data to password and message fields and checkout console output</li>
+	<li>as output you should get object with message and password keys values that you've entered</li>
 </ul>
--- src/components/new_message.js
+++ src/components/new_message.js
@@ -3,17 +3,44 @@ import React, { Component } from 'react';
 
 // Form for new message creating
 export default class NewMessage extends Component {
+	constructor() {
+		super();
+
+		this.state = { password: '', message: '' };
+
+		this.onChangeMessage = this.onChangeMessage.bind(this);
+		this.onChangePassword = this.onChangePassword.bind(this);
+		this.onSubmitMessage = this.onSubmitMessage.bind(this);
+	}
+
+	onChangeMessage(e) {
+		this.setState({ message: e.target.value });
+	}
+
+	onChangePassword(e) {
+		this.setState({ password: e.target.value });
+	}
+
+	onSubmitMessage(e) {
+		e.preventDefault();
+		console.log({ message: this.state.message, password: this.state.password });
+	}
+
 	render() {
 		return (
 			<div>
-				<form>
+				<form onSubmit={this.onSubmitMessage}>
 					<label>Message</label>
 					<textarea
+						onChange={this.onChangeMessage}
+						value={this.state.message}
 						className="form-control"
 						placeholder="Enter your message here"/>
 					<br/>
 					<label>Password</label>
 					<input
+						onChange={this.onChangePassword}
+						value={this.state.password}
 						type="password"
 						className="form-control"
 						placeholder="Enter your password here"/>
