9a5f415d16c79a54c4272c04dc31f031c86ee73b
AlexPurhalo <alexpurhalo@gmail.com>
2016-10-13 17:26:13 +0300

    message-decrypting

--- README.md
+++ README.md
@@ -11,4 +11,6 @@
 	<li>see that form got disappeared giving link to recently created message</li>
 	<li>follow by link and check that you in right place</li>
 	<li>check encrypted message by visited link</li>
+	<li>enter incorrect password and ensure that you see error</li>
+	<li>enter a correct password and ensure that your were shown decrypted message</li>
 </ul>
--- src/components/single_message.js
+++ src/components/single_message.js
@@ -8,14 +8,66 @@ import { readMessage } from '../actions/messages';
 
 // Shows message's data
 class SingleMessage extends Component {
+	constructor() {
+		super();
+
+		this.state = { password: '', showPassword: false, error: false };
+
+		this.handleChangePassword = this.handleChangePassword.bind(this);
+		this.handleSubmitPassword = this.handleSubmitPassword.bind(this);
+	}
+
 	componentWillMount() {
 		this.props.readMessage(this.props.params.id)
+	}
+
+	handleChangePassword(e) {
+		this.setState({ password: e.target.value });
+	}
+
+	handleSubmitPassword(e) {
+		e.preventDefault();
+		let decryptedMessage = this.decryptMessage(this.props.message.body, this.state.password);
+
+		if (decryptedMessage.length > 0) {
+			this.setState({ showPassword: !this.state.showPassword });
+			this.setState({ error: false })
+		} else {
+			this.setState({ error: true })
+		}
+
+	}
+
+	decryptMessage(encryptedString, password) {
+		return CryptoJS.AES.decrypt(encryptedString, password).toString(CryptoJS.enc.Utf8);
+	}
+
+	messageRender() {
+		return <div>message: {this.decryptMessage(this.props.message.body, this.state.password)}</div>;
+	}
+
+	passwordFormRender() {
+		return (
+			<form onSubmit={this.handleSubmitPassword}>
+				<label>Password</label>
+				<input onChange={this.handleChangePassword}
+					type="password"
+					className="form-control"
+					placeholder="Enter your password here"/>
+				<button
+					type="submit"
+					className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		);
 	}
 
 	render() {
 		return (
 			<div>
-				{this.props.message.body}
+				{this.state.showPassword ? this.messageRender() : this.passwordFormRender() }
+				{this.state.error ? (<span>Wrong Password</span>) : null}
 			</div>
 		);
 	}
